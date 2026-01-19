2173b89cd996f03c55b6b0f4d0784752f6649ea8
Kangkai Ye <ye.kangkai@dataimpact.io>
2026-01-19 16:25:09 +0100

    fix static

--- 42_Piscine_Django/d04/ex01/ex01/views.py
+++ 42_Piscine_Django/d04/ex01/ex01/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 
 def django_page(request):
     """Display Django introduction page."""
-    return render(request, 'ex01/django.html')
+    return render(request, 'ex01/django.html', {'page_name': 'django'})
 
 
 def affichage_page(request):
     """Display static page process explanation."""
-    return render(request, 'ex01/affichage.html')
+    return render(request, 'ex01/affichage.html', {'page_name': 'affichage'})
 
 
 def templates_page(request):
     """Display template engine explanation."""
-    return render(request, 'ex01/templates.html')
+    return render(request, 'ex01/templates.html', {'page_name': 'templates'})
